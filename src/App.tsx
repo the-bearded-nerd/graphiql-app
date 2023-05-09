34966100bc716a22b4b51b24cb4f536c37b08461
the-bearded-nerd <dmitrykarvonen@gmail.com>
2023-05-09 17:41:00 +0300

    init: add basic config and some deps

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Suspense } from 'react';
+
+import LangSwitch from './components/LangSwitch/LangSwitch';
+
+function App() {
+    return (
+        <div className="app">
+            <Suspense fallback="">
+                <div className="app-content">
+                    <LangSwitch />
+                </div>
+            </Suspense>
+        </div>
+    );
+}
+
+export default App;
