1e11a165385368def361f631746c5d33a215c3e8
Alexandra Samoilova <avsamoilava@gmail.com>
2023-05-11 14:40:58 +0300

    feat: add links in greeting page

--- src/pages/GreetingPage/GreetingPage.tsx
+++ src/pages/GreetingPage/GreetingPage.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useTranslation } from 'react-i18next';
+import { auth } from '../../firebase';
+import { Link } from 'react-router-dom';
 
 export const GreetingPage = () => {
   const { t } = useTranslation();
+  const [user] = useAuthState(auth);
   return (
     <div className="">
       <h1>{t('Greeting!')}</h1>
+      {user ? (
+        <Link to="/main">{t('на главную')}</Link>
+      ) : (
+        <Link to="/auth">{t('Войти')}</Link>
+      )}
     </div>
   );
 };
