00ae31c8e3f4dfb98dc5a4f34b9b25a6354991ea
Alexandra Samoilova <avsamoilava@gmail.com>
2023-05-11 14:42:36 +0300

    feat: add navigate in auth page; transfer log out btn

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import { Suspense } from 'react';
 import LangSwitch from './components/LangSwitch/LangSwitch';
 import AppRouter from './router/AppRouter';
 import { BrowserRouter } from 'react-router-dom';
+import { logout } from './firebase';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t } = useTranslation();
   return (
     <BrowserRouter>
       <div className="app">
         <Suspense fallback="">
           <div className="app-content">
             <LangSwitch />
+            <button onClick={logout}>{t('выйти')}</button>
             <AppRouter />
           </div>
         </Suspense>
--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import cl from './AuthPage.module.scss';
 import { SignIn, SignUp } from '../../components/Authorization';
-import { logout } from '../../firebase';
+import { auth } from '../../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 
 export const AuthPage = () => {
+  const [user] = useAuthState(auth);
   const { t } = useTranslation();
   const [activeTab, setActiveTab] = useState('sign in');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    user && navigate('/main');
+  }, [navigate, user]);
+
   return (
     <div className={cl['container']}>
-      <button onClick={logout}>{t('logout')}</button>
       <div className="tabs">
         <button onClick={() => setActiveTab('sign in')}>{t('Войти')}</button>
         <button onClick={() => setActiveTab('sign up')}>
