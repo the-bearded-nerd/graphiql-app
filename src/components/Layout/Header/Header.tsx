52abd3afdf8885e9113216a7c435ec936f511854
Alexandra Samoilova <avsamoilava@gmail.com>
2023-05-11 18:15:40 +0300

    feat: add sticky header

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -2,11 +2,27 @@ import cl from './Header.module.scss';
 import LangSwitch from '../../LangSwitch/LangSwitch';
 import { logout } from '../../../firebase';
 import { useTranslation } from 'react-i18next';
+import { useEffect, useRef, useState } from 'react';
 
 export const Header = () => {
   const { t } = useTranslation();
+  const headerRef = useRef<HTMLElement>(null);
+  const [prevPos, setPrevPos] = useState(window.pageYOffset);
+
+  useEffect(() => {
+    const scrollHandler = () => {
+      const currentPos = window.pageYOffset;
+      const el = headerRef.current as HTMLElement;
+      el.style.height = prevPos > currentPos ? '60px' : '40px';
+      el.style.backgroundColor = prevPos > currentPos ? '#ffffff' : '#0d3800';
+      setPrevPos(currentPos);
+    };
+    window.addEventListener('scroll', scrollHandler);
+    return () => window.removeEventListener('scroll', scrollHandler);
+  }, [prevPos]);
+
   return (
-    <header className={cl['header']}>
+    <header className={cl['header']} ref={headerRef}>
       <div className="container container_horizontal">
         <LangSwitch />
         <button onClick={logout}>{t('выйти')}</button>
