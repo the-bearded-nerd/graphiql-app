030c606f8dca465202e586e7c39203debcab5f26
Alexandra Samoilova <avsamoilava@gmail.com>
2023-05-11 11:37:23 +0300

    fix: add export for "auth"

--- src/firebase.ts
+++ src/firebase.ts
@@ -27,7 +27,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app);
 const db = getFirestore(app);
 
 export const registerWithEmailAndPassword = async (
