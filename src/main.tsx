7aefedd3a9e3cd1496f3cdfe8028b8aa0380e832
Alexandra Samoilova <avsamoilava@gmail.com>
2023-05-26 07:05:45 +0300

    feat: add mantine in project

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.scss';
-
+import { MantineProvider } from '@mantine/core';
 import './config/i18n/i18n';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
