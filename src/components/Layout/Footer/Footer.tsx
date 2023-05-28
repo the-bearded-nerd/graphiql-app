4ddb3c0543759887e0fd4acfc73290ab2e31e49c
Alexandra Samoilova <avsamoilava@gmail.com>
2023-05-28 20:56:40 +0300

    feat: add description on greeting page

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,19 +1,39 @@
 import { useTranslation } from 'react-i18next';
 import { RssIcon } from '../../Icons';
 import { Flex, Footer, createStyles } from '@mantine/core';
+import { IconBrandGithub } from '@tabler/icons-react';
 
 const useStyles = createStyles((theme) => ({
   inner: {
     [theme.fn.smallerThan('sm')]: {
       flexDirection: 'column',
+      gap: 20,
     },
   },
   link: {
     color: 'inherit',
     textDecoration: 'none',
+    display: 'flex',
+    gap: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontWeight: 500,
+    transition: 'transform 0.2s',
     '&:visited': {
       color: 'inherit',
       textDecoration: 'none',
+    },
+    '&:hover': {
+      transform: 'scale(1.1)',
+    },
+    [theme.fn.smallerThan('sm')]: {
+      width: '100%',
+    },
+  },
+  logoLink: {
+    transition: 'transform 0.2s',
+    '&:hover': {
+      transform: 'scale(1.1)',
     },
   },
 }));
@@ -30,16 +50,21 @@ export const FooterCustom = () => {
         m={'0 auto'}
         className={classes.inner}
       >
-        <a href="https://rs.school/react/" target="_blank">
+        <a
+          href="https://rs.school/react/"
+          target="_blank"
+          className={classes.logoLink}
+        >
           <RssIcon />
         </a>
         2023
-        <Flex wrap={'wrap'} align={'center'} gap={15}>
+        <Flex wrap={'wrap'} align={'center'} gap={25}>
           <a
             href="https://github.com/avsamoilava"
             target="_blank"
             className={classes.link}
           >
+            <IconBrandGithub />
             {t('Саша')}
           </a>
           <a
@@ -47,6 +72,7 @@ export const FooterCustom = () => {
             target="_blank"
             className={classes.link}
           >
+            <IconBrandGithub />
             {t('Дима')}
           </a>
         </Flex>
